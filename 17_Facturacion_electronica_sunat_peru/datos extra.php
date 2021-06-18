31f2e33dd0f56c54f0532eacb4270bd53234cfe0
Pc_gamer_no-admin <phool_05@hotmail.com>
2021-06-18 11:07:28 -0500

    version004

---
+++ 17_Facturacion_electronica_sunat_peru/datos extra.php
@@ -0,0 +1,28 @@
+//Greenter libreria para facturacion electronica SUNAT
+    Greenter Facturación Electrónica
+    https://www.youtube.com/watch?v=u69SWWdiReQ
+    https://github.com/thegreenter/greenter
+    //Empieza a explicar el funcionamiento del codigo en herokuapp
+    https://youtu.be/u69SWWdiReQ?t=1765
+
+//Greenter contactar
+    https://community.greenter.dev/
+    https://greenter.dev/?fbclid=IwAR2Ze7xkXwhiUqILOHfwOB90z3QRnaj5l2o1C9u9RIcvcz-kUP-QFoyQmtQ
+    https://fe-primer.greenter.dev/docs/
+    https://greenter-sample.herokuapp.com/
+
+//Curso 2021 $/300 soles
+    php puro sin framework
+    https://www.facebook.com/ceti.chiclayo.5/posts/1939188226222634   --->2021
+    https://fb.watch/2-3aZYkBbF/                                      --->2020 -- 15 de junio
+    https://www.facebook.com/ceti.chiclayo.5/posts/1779861188822006   --->2020 -- 28 de julio
+    https://www.facebook.com/ceti.chiclayo.5/posts/1799608436847281   --->2020 -- 19 de agosto
+    https://www.facebook.com/ceti.chiclayo.5/posts/1814710322003759   --->2020 -- 5  de setiembre
+    https://www.facebook.com/ceti.chiclayo.5/posts/1898970410244416   --->2020 -- 23 de noviembre
+    https://www.facebook.com/ceti.chiclayo.5/posts/1914340935374030   --->2020 -- 10 de diciembre
+    https://www.facebook.com/ceti.chiclayo.5/posts/1856173671190757   --->2020 -- 12 de octubre
+
+//Para complementar o pedir ayuda le pedimos ayuda a freddy rojas
+
+
+//Tengo en mis archivos privados el codigo fuente :v
